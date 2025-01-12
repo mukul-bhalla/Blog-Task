a8ea46f1b60f3a2a2e328d8e7b879df8936d8955
mukul-bhalla <bhallamukul27@gmail.com>
2025-01-12 19:30:16 +0530

    DB Config

--- api/index.js
+++ api/index.js
@@ -1,7 +1,21 @@
 import express from 'express'
+import mongoose from 'mongoose'
 
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+mongoose.connect(process.env.DB_URL)
+    .then(() => {
+        console.log("DB Connected");
+    })
+    .catch((e) => {
+        console.log(e);
+        console.log("Mongo Error")
+    })
 const app = express();
 
 app.listen(3000, () => {
     console.log("Server running on port 3000")
 })
+// 3cvzunCDu44V5pZV
