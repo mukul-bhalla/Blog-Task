a3f39a539a3be5c7c6b906133fe16898fac1994c
mukul-bhalla <bhallamukul27@gmail.com>
2025-01-12 18:35:25 +0530

    Header Component

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React from 'react';
 import { Button, Navbar, TextInput } from "flowbite-react";
 import { Link } from 'react-router-dom';
-import { AiOutlineSearch } from 'react-icons/ai'
+import { AiOutlineSearch } from 'react-icons/ai';
+import { FaMoon } from 'react-icons/fa';
 export default function Header() {
     return (
         <Navbar className='border-b-2'>
@@ -9,17 +10,22 @@ export default function Header() {
                 <span className='px-2 py-1 bg-pink-500 rounded-lg dark:text-white'>MERN</span>
                 Blog
             </Link>
-            <form>
-                <TextInput
-                    type='text'
-                    placeholder='Search...'
-                    rightIcon={AiOutlineSearch}
-                    className='hidden lg:inline'
-                />
-            </form>
-            <Button className='w-12 h-10 lg:hidden' color='gray'>
+            <Button className='w-12 h-10 lg:hidden' color='gray' pill>
                 <AiOutlineSearch />
             </Button>
+            <div className='flex gap-2 md:order-2' color='gray'>
+                <Button className='w-12 h-10 ' color='black'>
+                    <FaMoon />
+                </Button>
+                <Link to="/register">
+                    <Button className="" gradientDuoTone='purpleToBlue'>
+                        Sign In
+                    </Button>
+                </Link>
+
+
+
+            </div>
         </Navbar>
     )
 }
