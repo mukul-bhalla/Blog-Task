a45e01fa1db6b03dcb5452af1dd3380bab8efb73
mukul-bhalla <bhallamukul27@gmail.com>
2025-01-12 19:54:13 +0530

    Router and Controller Setup

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
-
+import userRoutes from './routes/user.route.js';
 dotenv.config();
 
 mongoose.connect(process.env.DB_URL)
@@ -14,6 +14,8 @@ mongoose.connect(process.env.DB_URL)
     })
 
 const app = express();
+
+app.use('/api/user', userRoutes)
 
 app.listen(3000, () => {
     console.log("Server running on port 3000")
